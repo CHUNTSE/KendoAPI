fef9c3da9b29fd8120badbdecbcd42a257b9c524
CHUNTSE <ex29872678@gmail.com>
2018-06-18 23:25:37 +0800

    new

--- KendoGrid/KendoGrid/Controllers/HomeController.cs
+++ KendoGrid/KendoGrid/Controllers/HomeController.cs
@@ -6,10 +6,16 @@ using System.Web.Mvc;
 
 namespace KendoGrid.Controllers
 {
+    [RoutePrefix("KendoUI")]
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("")]
+        public ActionResult IndexKendo()
         {
             return View();
         }
