34a2b1c74440677076f33cfb62f6fc4c0d7af3ef
CHUNTSE <ex29872678@gmail.com>
2018-06-20 20:33:23 +0800

    new

--- KendoAPI/KendoAPI/Controllers/ValuesController.cs
+++ KendoAPI/KendoAPI/Controllers/ValuesController.cs
@@ -25,98 +25,99 @@ namespace KendoAPI.Controllers
                      .ConnectionStrings["db"]
                      .ConnectionString);
 
-            var items = db.Query<Products>("SELECT * FROM Car_Brand ORDER BY Id DESC");
-            //"SELECT * FROM Products ORDER BY ProductID DESC");
+            var items = db.Query<Products>(
+                //"SELECT * FROM Car_Brand ORDER BY Id DESC");
+            "SELECT * FROM Products ORDER BY ProductID DESC");
 
             return items;
         }
 
-        //[Route("{id}")]
-        //[HttpGet]
-        //public IHttpActionResult Get(string id)
-        //{
-        //    var db = new SqlConnection(
-        //             ConfigurationManager
-        //             .ConnectionStrings["db"]
-        //             .ConnectionString);
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult Get(string id)
+        {
+            var db = new SqlConnection(
+                     ConfigurationManager
+                     .ConnectionStrings["db"]
+                     .ConnectionString);
 
-        //    var items = db.Query<Products>(
-        //        "SELECT * FROM Products WHERE ProductID = @id", new
-        //        {
-        //            id = id
-        //        });
+            var items = db.Query<Products>(
+                "SELECT * FROM Products WHERE ProductID = @id", new
+                {
+                    id = id
+                });
 
-        //    var item = items.FirstOrDefault();
+            var item = items.FirstOrDefault();
 
-        //    if (item == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    else
-        //    {
-        //        return Content(HttpStatusCode.OK, item);
-        //    }
-        //}
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Content(HttpStatusCode.OK, item);
+            }
+        }
 
-        //[Route("")]
-        //[HttpPost]
-        //public void Post([FromBody] Products item)
-        //{
-        //    var db = new SqlConnection(
-        //             ConfigurationManager
-        //             .ConnectionStrings["db"]
-        //             .ConnectionString);
+        [Route("")]
+        [HttpPost]
+        public void Post([FromBody] Products item)
+        {
+            var db = new SqlConnection(
+                     ConfigurationManager
+                     .ConnectionStrings["db"]
+                     .ConnectionString);
 
-        //    db.Execute(
-        //        "INSERT INTO Products (ProductID, CategoryID, ProductName, Stock, UnitPrice, Size, Color, Instructions, Path)" +
-        //        "VALUES (@productID, @categoryID, @productName, @stock, @unitPrice, @size, @color, @instructions, @path)", new
-        //        {
-        //            productID = item.ProductID,
-        //            categoryID = item.CategoryID,
-        //            productName = item.ProductName,
-        //            stock = item.Stock,
-        //            unitPrice = item.UnitPrice,
-        //            size = item.Size,
-        //            color = item.Color,
-        //            instructions = item.Instructions,
-        //            path = item.Path
-        //        });
-        //}
+            db.Execute(
+                "INSERT INTO Products (ProductID, CategoryID, ProductName, Stock, UnitPrice, Size, Color, Instructions, Path)" +
+                "VALUES (@productID, @categoryID, @productName, @stock, @unitPrice, @size, @color, @instructions, @path)", new
+                {
+                    productID = item.ProductID,
+                    categoryID = item.CategoryID,
+                    productName = item.ProductName,
+                    stock = item.Stock,
+                    unitPrice = item.UnitPrice,
+                    size = item.Size,
+                    color = item.Color,
+                    instructions = item.Instructions,
+                    path = item.Path
+                });
+        }
 
-        //[Route("{id}/productName")]
-        //[HttpPatch]
-        //public void UpdateTodoTitle(string id, [FromBody] Products item)
-        //{
-        //    var db = new SqlConnection(
-        //             ConfigurationManager
-        //             .ConnectionStrings["db"]
-        //             .ConnectionString);
+        [Route("{id}")]
+        [HttpPatch]
+        public void UpdateTodoTitle(string id, [FromBody] Products item)
+        {
+            var db = new SqlConnection(
+                     ConfigurationManager
+                     .ConnectionStrings["db"]
+                     .ConnectionString);
 
-        //    db.Execute(
-        //        "UPDATE Products " +
-        //        "SET ProductName = @productName " +
-        //        "WHERE ProductID = @id", new
-        //        {
-        //            id,
-        //            productName = item.ProductName
-        //        });
-        //}
+            db.Execute(
+                "UPDATE Products " +
+                "SET ProductName = @productName " +
+                "WHERE ProductID = @productID", new
+                {
+                    productID = id,
+                    productName = item.ProductName
+                });
+        }
 
-        //[Route("{id}")]
-        //[HttpDelete]
-        //public void Delete(string id)
-        //{
-        //    var db = new SqlConnection(
-        //             ConfigurationManager
-        //             .ConnectionStrings["db"]
-        //             .ConnectionString);
+        [Route("{id}")]
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            var db = new SqlConnection(
+                     ConfigurationManager
+                     .ConnectionStrings["db"]
+                     .ConnectionString);
 
-        //    db.Execute(
-        //        "DELETE FROM Products " +
-        //        "WHERE ProductID = @id", new
-        //        {
-        //            id = id,
-        //        });
-        //}
+            db.Execute(
+                "DELETE FROM Products " +
+                "WHERE ProductID = @productID", new
+                {
+                    productID = id,
+                });
+        }
     }
 }
